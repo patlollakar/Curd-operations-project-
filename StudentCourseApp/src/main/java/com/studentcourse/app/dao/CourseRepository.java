7d0d1349f2f08736cb522c1905a38b05069a37d0
Karthik <Karthik@LAPTOP-HPI08USJ>
2017-10-18 11:00:31 +0500

    project CURD operations

--- StudentCourseApp/src/main/java/com/studentcourse/app/dao/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.studentcourse.app.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.studentcourse.app.model.Course;
-
-public interface CourseRepository extends JpaRepository<Course, String> {
-
-}
